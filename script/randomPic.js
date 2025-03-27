2b54324df703bd807dfd34bfb59ecd4bc8ccc860
Wakana <pre.kuro.05@gmail.com>
2025-03-27 14:44:32 +0100

    random pic

---
+++ script/randomPic.js
@@ -0,0 +1,31 @@
+var urls = [
+  'url(./img/0.jpg)',
+  'url(./img/1.jpg)',
+  'url(./img/2.jpg)',
+  'url(./img/3.jpg)',
+  'url(./img/4.jpg)',
+  'url(./img/5.jpg)',
+  'url(./img/6.jpg)',
+  'url(./img/7.jpg)',
+  'url(./img/8.jpg)',
+  'url(./img/9.jpg)',
+  'url(./img/10.jpg)',
+];
+
+
+
+
+
+function myFunction(){
+  try{ 
+    
+    var active = Math.floor(Math.random() * (urls.length));
+    document.body.style.backgroundImage = urls[active];
+    
+  }catch(error){
+        console.log(error);
+  }
+ 
+}
+
+document.getElementById('screen').addEventListener('click',myFunction);
