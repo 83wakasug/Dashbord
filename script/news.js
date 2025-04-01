2ffcdb08239fd09812b71706e532e8bec77d8a36
Wakana <pre.kuro.05@gmail.com>
2025-04-01 15:02:15 +0200

    add news

---
+++ script/news.js
@@ -0,0 +1,27 @@
+const key = "964303efb75b4e2aa94fec658d29d853";
+const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${key}`;
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    displayNews(data.articles);
+  })
+  .catch(error => console.error("Error fetching news:", error));
+
+function displayNews(articles) {
+  const newsContainer = document.getElementById("add_news");
+  newsContainer.innerHTML = "<h3>Latest News</h3>"; 
+
+  articles.forEach(article => {
+    const newsItem = document.createElement("div");
+    newsItem.classList.add("news-item");
+
+    newsItem.innerHTML = `
+      <a href="${article.url}" target="_blank">
+        <h4>${article.title}</h4>
+      </a>
+      <p>${article.description || "No description available"}</p>
+    `;
+    newsContainer.appendChild(newsItem);
+  });
+}
